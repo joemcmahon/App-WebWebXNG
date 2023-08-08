b30fd6fc2aff95aac122fba990d61335b8cce3d4
Joe McMahon <joe.mcmahon@gmail.com>
2023-08-07 22:28:43 -0700

    Move PasswordManager, fix critic issues
     - Add strict and warnings at the top
     - Convert block comments to POD
     - remove bareword filehandles
     - fix numerous scoping issues
     - clean up trailing whitespace
     - Move to App::WebWebXNG namespace

--- lib/App/WebWebXNG/AuthManager.pm
+++ lib/App/WebWebXNG/AuthManager.pm
@@ -1,12 +1,15 @@
-package PasswordManager;
+use strict;
+use warnings;
+
+package App::WebWebXNG::AuthManager;
 
 use File::Copy;
 
 =head1 NAME
 
-PasswordManager - Add, update and delete user accounts.
+App::WebWebXNG::AuthManager - Add, update and delete user accounts.
 
-=head1 DESCRIPTION
+=head1 SYNOPSIS
 
     $admin = new PasswordManager("/path/to/my/file");
     die "Can't use password file: $why"
@@ -37,20 +40,31 @@ PasswordManager - Add, update and delete user accounts.
         or die "Couldn't delete JaneDoe: " , $admin->unusable();
 
     # Various verifications.
-    die "bad user name: $why" 
+    die "bad user name: $why"
         if $why = verify_name($account_name);
-    die "bad password: $why" 
+    die "bad password: $why"
         if $why = verify_pwd($account_name);
 
     # set/reset attributes
     $admin->attr_add("JaneDoe","manager");
     $admin->attr_del("JoeBlow","adduser");
 
-=cut
+=head1 DESCRIPTION
 
-#-------------------------------------------------------------------------
-# new(FILE) - Construct new object using the supplied password file.
-#-------------------------------------------------------------------------
+This class manages the users for WebWebX, including validation and attributes.
+
+The current version of the class manages a cgi-bin basic authentication
+password file. Obviously we'll want something better than this, based on the
+Mojolicious authentication plugin, but the basic interface to the class is
+sound.
+
+=head1 CLASS METHODS
+
+=head2  new(FILE)
+
+Construct new object using the supplied password file.
+
+=cut
 
 sub new {
 
@@ -74,7 +88,7 @@ sub new {
    elsif (!-r $file or !-w _) {
        $self->unusable("file $file is not both readable and writable");
    }
-   elsif (!-r $dir or !-w _) { 
+   elsif (!-r $dir or !-w _) {
        $self->unusable("directory $dir is not both readable and writable");
    }
    else {  # usable.
@@ -84,19 +98,25 @@ sub new {
    $self;
 }
 
-#-------------------------------------------------------------------------
-# unusable() - set/get why the file is unusable (if it is)
-#-------------------------------------------------------------------------
+=head1 INSTANCE METHODS
+
+=head2 unusable()
+
+Set/get why the file is unusable (if it is)
+
+=cut
 
 sub unusable {
     my ($self, $why) = @_;
-    $self->{Failure} = $why if defined $why; 
+    $self->{Failure} = $why if defined $why;
     $self->{Failure};
 }
 
-#-------------------------------------------------------------------------
-# add(NAME,PASSWD) - add a new password file entry 
-#-------------------------------------------------------------------------
+=head2 add(NAME,PASSWD)
+
+Add a new password file entry
+
+=cut
 
 sub add {
 
@@ -108,22 +128,25 @@ sub add {
 
    return 0 unless($self->_lock);
 
-   unless (open HTPASSWD, ">>$passwdfile") {
+   my $htpasswd;
+   unless (open $htpasswd, ">>", "$passwdfile") {
        $self->unusable("Password file $passwdfile could not be opened: $!");
        return 0;
    }
    my $attrs = join(",",@attrs);
-   print HTPASSWD join(":",$name,$encrypted,$attrs),"\n";
-   close HTPASSWD;
-   
+   print $htpasswd join(":",$name,$encrypted,$attrs),"\n";
+   close $htpasswd;
+
    $self->_unlock;
 
    return 1;
 }
 
-#-------------------------------------------------------------------------
-# exists(NAME) - check if user exists already 
-#-------------------------------------------------------------------------
+=head2 exists(NAME)
+
+Check if user exists already
+
+=cut
 
 sub exists {
 
@@ -132,35 +155,38 @@ sub exists {
 
    $self->unusable(0);
 
-   unless (open HTPASSWD, "$passwdfile") {
+   my $htpasswd;
+   unless (open $htpasswd, "<", $passwdfile) {
        $self->unusable("Password file $passwdfile could not be opened: $!");
    }
 
-   my $name;
-   my $password;
    my $found = 0;
-   while (<HTPASSWD>) {
+   my($name, $password, $attrs);
+   while (<$htpasswd>) {
        chomp;
-       ($name,$password,$attrs) = split /:/, $_;
+       ($name, $password, $attrs) = split /:/, $_;
        if ($name eq $username) {
 	   $found = 1;
 	   last;
        }
    }
-   close HTPASSWD;
+   close $htpasswd;
    my @attrs = (defined $attrs ? split(/,/,$attrs) : ());
-   return $found ? ($name,$password,@attrs) : ();
+   return $found ? ($name, $password, @attrs) : ();
 }
 
-#-------------------------------------------------------------------------
-# update(USER,PASSWORD,ATTRS) - change password and/or attributes
-#-------------------------------------------------------------------------
+=head2 update(USER,PASSWORD,ATTRS)
+
+Change password and/or attributes
+
+=cut
 
 sub update {
    my $self = shift;
    my ($username, $newpasswd, @newattrs) = @_;
-   
-   my $encrypted = $self->_encrypt($newpasswd) if $newpasswd;
+
+   my $encrypted;
+   $encrypted = $self->_encrypt($newpasswd) if $newpasswd;
    my $passwdfile = $self->{File};
    my $newattrs = join(",",@newattrs);
    $newattrs = "" unless defined $newattrs;
@@ -169,43 +195,47 @@ sub update {
 
    return 0 unless $self->_lock;
 
-   my $tmp = $self->{Dir}.".htpasswdtmp";
-   unless(open TMP, ">".$tmp) {
-      $self->unusable("couldn't open $tmp: $!");
+   my $tmpfile = $self->{Dir}.".htpasswdtmp";
+   my $tmp;
+   unless(open $tmp, ">", $tmpfile) {
+      $self->unusable("couldn't open $tmpfile: $!");
       return 0;
    }
 
-   unless(open HTPASSWD, "$passwdfile") {
+   my $htpasswd;
+   unless(open $htpasswd, "<", "$passwdfile") {
       $self->unusable("couldn't open $passwdfile: $!");
       return 0;
    }
 
    my ($name, $passwd, $attrs);
 
-   while (<HTPASSWD>) {
+   while (<$htpasswd>) {
       chomp;
       ($name,$passwd,$attrs) = split /:/;
       $attrs ||= "";
-      ($name ne $username) ? print TMP
-                           : print TMP join(":",$username,
-                                               ($encrypted 
-                                                  ?  $encrypted : $passwd),
-                                                (defined $newattrs
+      ($name ne $username) ? print $tmp
+                           : print $tmp join(":",$username,
+                                               ($encrypted
+                                                  ? $encrypted : $passwd),
+                                               (defined $newattrs
                                                   ? $newattrs : $attrs)
                                            );
-       print TMP "\n";
+       print $tmp "\n";
    }
-   close HTPASSWD;
-   close TMP;
+   close $htpasswd;
+   close $tmp;
 
    move($tmp, $passwdfile);
    $self->_unlock;
    1;
 }
 
-#-------------------------------------------------------------------------
-# delete(USER) - delete a user 
-#-------------------------------------------------------------------------
+=head2 delete(USER)
+
+Delete a user
+
+=cut
 
 sub delete {
 
@@ -216,44 +246,49 @@ sub delete {
 
    $self->_lock;
 
-   my $tmp = $self->{Dir}.".htpasswdtmp";
-   unless(open TMP, ">".$tmp) {
-      $self->unusable("couldn't open $tmp: $!");
+   my $tmpfile = $self->{Dir}.".htpasswdtmp";
+   my $tmp;
+   unless(open $tmp, ">", $tmpfile) {
+      $self->unusable("couldn't open $tmpfile: $!");
       return 0;
    }
 
-   unless(open HTPASSWD, "$passwdfile") {
+   my $htpasswd;
+   unless(open $htpasswd, "<", $passwdfile) {
       $self->unusable("couldn't open $passwdfile: $!");
       return 0;
    }
 
-   while (<HTPASSWD>) {
-      print TMP unless /^$username:/;
+   while (<$htpasswd>) {
+      print $tmp unless /^$username:/;
    }
-   close HTPASSWD;
-   close TMP;
+   close $htpasswd;
+   close $tmp;
 
-   # Finished updating TMP file, now rename tmp to .htpasswd.
+   # Finished updating temp file, now rename tmpfile to .htpasswd.
    move($tmp,$passwdfile);
 
    $self->_unlock;
    1;
 }
 
-#-------------------------------------------------------------------------
-# verify(USER,PASSWORD) - verify a user is a valid one
-#   returns: 1 - OK
-#            0 - not OK
-#        undef - couldn't check
-#-------------------------------------------------------------------------
+=head2 verify(USER,PASSWORD)
+
+Verify a user is valid
+
+Returns: 1 - OK
+         0 - not OK
+         undef - couldn't check
+
+=cut
 
 sub verify {
     my ($self, $user, $pw) = @_;
     my $passwdfile         = $self->{File};
     my ($lookup,$opw)      = $self->exists($user);
 
-    return undef if $self->unusable();
-    
+    return if $self->unusable();
+
     unless (defined $opw) {
         $self->unusable("User $user not found");
         return 0;
@@ -267,10 +302,12 @@ sub verify {
         return 0;
     }
 }
-   
-#-------------------------------------------------------------------------
-# users() - return all current users
-#-------------------------------------------------------------------------
+
+=head2 users()
+
+Return all current users
+
+=cut
 
 sub users {
    my ($self) = @_;
@@ -278,41 +315,49 @@ sub users {
 
    $self->unusable(0);
 
-   unless (open HTPASSWD, "$passwdfile") {
+   my $htpasswd;
+   unless (open $htpasswd, "<", $passwdfile) {
        $self->unusable("Password file $passwdfile could not be opened: $!");
    }
 
    my $name;
    my $password;
    my @users = ();
-   while (<HTPASSWD>) {
+   while (<$htpasswd>) {
       ($name,$password) = split /:/, $_;
       push @users,$name;
    }
-   close HTPASSWD;
+   close $htpasswd;
    @users;
 }
 
-#-------------------------------------------------------------------------
-# verify_name(NAME)
-#-------------------------------------------------------------------------
+=head2 verify_name(NAME)
+
+Validate name meets requirements. We should make this a bit more stringent
+so we get only names that are valid wiki link names.
+
+=cut
 
 sub verify_name {
 
    my ($self, $account) = @_;
 
    # Make sure we only have .htpasswd safe characters.
-   (length($account) < 15) or 
+   (length($account) < 15) or
       return "must be 14 or fewer characters.";
-   ($account =~ /^[a-z0-9-_]+$/i )  or 
+   ($account =~ /^[a-z0-9-_]+$/i )  or
       return "may contain only alphabetics, numerics, and underscores.";
    0;
 
 }
 
-#-------------------------------------------------------------------------
-# verify_password(PASSWORD)
-#-------------------------------------------------------------------------
+=head2 verify_password(PASSWORD)
+
+Validate that the password is not unreasonable.
+
+=cut
+
+# XXX: wow, that was sure a more innocent time.
 
 sub verify_password {
 
@@ -325,11 +370,13 @@ sub verify_password {
 # _encrypt(PASSWORD,[SALT])
 #-------------------------------------------------------------------------
 # encrypt a password
+#
+# XXX: absolutely too weak and has to be replaced.
 
 sub _encrypt {
 
    my ($self,$password,$salt) = @_;
-   
+
    # if salt wasn't supplied then generate it.
    # 62 is the size of the array ('0'..'9','a'..'z','A'..'Z'), so
    # [int(62 * rand)] randomly picks one element from the array.
@@ -339,13 +386,14 @@ sub _encrypt {
       unless $salt;
 
    crypt($password,$salt);
-	
 }
 
-#-------------------------------------------------------------------------
-#  attr_add
-#-------------------------------------------------------------------------
-# add one or more attributes to a user's entry.
+=head2  attr_add
+
+Add one or more attributes to a user's entry.
+
+=cut
+
 sub attr_add {
    my ($self, $name, @attrs) = @_;
 
@@ -355,7 +403,7 @@ sub attr_add {
 
    if ($oname) {
        my %attrs;
-       foreach my $attr (@oldattrs,@attrs) { 
+       foreach my $attr (@oldattrs,@attrs) {
           next unless defined $attr;
           $attrs{$attr}++;
        }
@@ -369,25 +417,27 @@ sub attr_add {
 
 }
 
-#-------------------------------------------------------------------------
-#  attr_del
-#-------------------------------------------------------------------------
-# add one or more attributes to a user's entry.
+=head2  attr_del
+
+Remove one or more attributes from a user's entry.
+
+=cut
+
 sub attr_del {
    my ($self, $name, @attrs) = @_;
 
    $self->unusable(0);
 
    my ($oname, $passwd, @oldattrs) = $self->exists($name);
-  
-   if ($oname) { 
+
+   if ($oname) {
        my %attrs;
        my $attr;
-       foreach $attr (@oldattrs) { $attrs{$attr}++ }
-       foreach $attr (@attrs)    { delete $attrs{$attr}}
+       foreach my $attr (@oldattrs) { $attrs{$attr}++ }
+       foreach my $attr (@attrs)    { delete $attrs{$attr}}
        @oldattrs = keys %attrs;
        return 1 if $self->update($name,"",@oldattrs);
-       $self->unusable("Could not delete attributes for $name: " 
+       $self->unusable("Could not delete attributes for $name: "
                        . $self->unusable());
        return 0;
    }
@@ -397,11 +447,13 @@ sub attr_del {
    }
 }
 
-#-------------------------------------------------------------------------
-#  has_attr
-#-------------------------------------------------------------------------
-#  return true or false, depending on whether a user has the specified
-#  attribute(s).
+=head2  has_attr
+
+Return true or false, depending on whether a user has the specified
+attribute(s).
+
+=cut
+
 sub has_attr {
    my ($self,$name,@attrs) = @_;
    my (%attrs,@not_found);
@@ -413,7 +465,7 @@ sub has_attr {
        foreach my $check (@attrs) {
            push @not_found, $check unless grep /^$check$/,@oattrs;
        }
-       $self->unusable("$name does not have: ",join(",",@not_found)) 
+       $self->unusable("$name does not have: ",join(",",@not_found))
            if int(@not_found);
        return ! int(@not_found);
    }
@@ -423,9 +475,8 @@ sub has_attr {
    }
 }
 
-#-------------------------------------------------------------------------
-#  _lock 
-#-------------------------------------------------------------------------
+# _lock
+#
 # Loosely based on example from the Perl Cookbook.
 # Use directory as a lock to avoid a race condition for the lock.
 
@@ -450,9 +501,7 @@ sub _lock {
 
 }
 
-#-------------------------------------------------------------------------
 # _unlock
-#-------------------------------------------------------------------------
 
 sub _unlock {
 
@@ -460,7 +509,5 @@ sub _unlock {
    rmdir($self->{File}."_lock");
 
 }
-
-#-------------------------------------------------------------------------
 
 1;
