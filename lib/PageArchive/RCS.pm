f7c0b44dfdda1a15fcce5a25daa49f2827713092
Joe McMahon <joe.mcmahon@gmail.com>
2023-08-11 12:29:39 -0700

    Tidy PageArchive::RCS

--- lib/PageArchive/RCS.pm
+++ lib/PageArchive/RCS.pm
@@ -44,55 +44,55 @@ Create a new PageArchive object:
 =cut
 
 sub new {
-    my ($class, $dirname, %params) = @_;
-    die "No page storage directory supplied" unless $dirname;
+  my ( $class, $dirname, %params ) = @_;
+  die "No page storage directory supplied" unless $dirname;
 
-    my $self = {};
-    bless $self,$class;
+  my $self = {};
+  bless $self, $class;
 
-    # Set up the File::LockDir callbacks.
-    $self->logger($params{Logger} || sub { print STDERR @_ });
-    $self->fatal($params{Fatal}   || sub { croak @_ });
+  # Set up the File::LockDir callbacks.
+  $self->logger( $params{Logger} || sub { print STDERR @_ } );
+  $self->fatal( $params{Fatal}   || sub { croak @_ } );
 
-    $self->setError();
+  $self->setError();
 
-    # Verify that dir name is OK; try to create it if we must;
-    # return undef if we can't.
-    unless (-d $dirname) {	# Not a dir or doesn't exist
-        unless (-e $dirname) {  # Doesn't exist
-            # Make the directory, including parent dirs.
-            # Return undef if we fail along the way.
-            # XXX: use File::Path!
-            my @path = split ('/',$dirname);
-            my $accumulated_path = "";
-            foreach (@path) {
-                $accumulated_path .= "/$_";
-                next if -e $accumulated_path and -d _;
-                unless (mkdir $accumulated_path,0700) {
-                    $self->fatal->("Can't create $accumulated_path: $!");
-                }
-            }
+  # Verify that dir name is OK; try to create it if we must;
+  # return undef if we can't.
+  unless ( -d $dirname ) {    # Not a dir or doesn't exist
+    unless ( -e $dirname ) {    # Doesn't exist
+                                # Make the directory, including parent dirs.
+                                # Return undef if we fail along the way.
+                                # XXX: use File::Path!
+      my @path             = split( '/', $dirname );
+      my $accumulated_path = "";
+      foreach (@path) {
+        $accumulated_path .= "/$_";
+        next if -e $accumulated_path and -d _;
+        unless ( mkdir $accumulated_path, 0700 ) {
+          $self->fatal->("Can't create $accumulated_path: $!");
         }
-        else { # Exists, but is not a directory
-            $self->fatal->("$dirname exists, but is not a directory");
-        }
+      }
+    } else {    # Exists, but is not a directory
+      $self->fatal->("$dirname exists, but is not a directory");
     }
-    else { # is a directory (and exists)
-        $self->{DirName} = $dirname;
-        $self->{LockLimit} = 30;
-    }
+  } else {    # is a directory (and exists)
+    $self->{DirName}   = $dirname;
+    $self->{LockLimit} = 30;
+  }
 
-    # Initialize File::LockDir.
-    $self->_locker(File::LockDir->new(
-        fatal  => $self->fatal,
-        logger => $self->logger,
-    ));
+  # Initialize File::LockDir.
+  $self->_locker(
+    File::LockDir->new(
+      fatal  => $self->fatal,
+      logger => $self->logger,
+    )
+  );
 
-    # See if we can read the directory; return undef if not.
-    unless(opendir($self->{Handle},$dirname)) {
-        $self->fatal->("Cannot open $dirname: $!");
-    }
-    return $self;
+  # See if we can read the directory; return undef if not.
+  unless ( opendir( $self->{Handle}, $dirname ) ) {
+    $self->fatal->("Cannot open $dirname: $!");
+  }
+  return $self;
 }
 
 =head2 DESTROY
@@ -102,8 +102,8 @@ Clean up and shut down
 =cut
 
 sub DESTROY {
-    my $self = shift;
-    closedir $self->{Handle} if defined $self->{Handle};
+  my $self = shift;
+  closedir $self->{Handle} if defined $self->{Handle};
 }
 
 =head1 INSTANCE METHODS
@@ -118,13 +118,13 @@ versions exist.
 =cut
 
 sub defined {
-    my($self, $name, $version) = @_;
-    $version = "" unless defined $version;
+  my ( $self, $name, $version ) = @_;
+  $version = "" unless defined $version;
 
-    # Search directory for filenames matching.
-    $self->dh_reset;
-    $self->{Rewound} = 0;
-    return grep(/^$name,$version/,readdir $self->{Handle});
+  # Search directory for filenames matching.
+  $self->dh_reset;
+  $self->{Rewound} = 0;
+  return grep( /^$name,$version/, readdir $self->{Handle} );
 }
 
 =head2 lock($name)
@@ -136,10 +136,11 @@ Returns undef if the entry can't be locked, true otherwise.
 =cut
 
 sub lock {
-    my ($self, $name, $who, $host) = @_;
-    my ($which,$locked_by)  =
-        $self->locker->nflock("$self->{DirName}/$name", $self->lock_limit(), $who, $host);
-    return ($which, $locked_by);
+  my ( $self, $name, $who, $host ) = @_;
+  my ( $which, $locked_by ) =
+    $self->locker->nflock( "$self->{DirName}/$name", $self->lock_limit(), $who,
+    $host );
+  return ( $which, $locked_by );
 }
 
 =head2 unlock($name)
@@ -151,8 +152,8 @@ Returns undef if the entry wasn't locked, true otherwise.
 =cut
 
 sub unlock {
-    my ($self, $name) = @_;
-    $self->locker->nfunlock("$self->{DirName}/$name");
+  my ( $self, $name ) = @_;
+  $self->locker->nfunlock("$self->{DirName}/$name");
 }
 
 =head2 is_unlocked($name)
@@ -165,8 +166,8 @@ Returns: (1, undef) if the entry is unlocked
 =cut
 
 sub is_unlocked {
-    my ($self,$name) = @_;
-    return $self->locker->nlock_state("$self->{DirName}/$name");
+  my ( $self, $name ) = @_;
+  return $self->locker->nlock_state("$self->{DirName}/$name");
 }
 
 =head2 max_version($name)
@@ -198,32 +199,33 @@ Finds the file, reads it, and uses Storable::inflate to restore it.
 =cut
 
 sub get {
-    my($self, $name, $version) = @_;
-    $version = $self->max_version($name) unless $version;
+  my ( $self, $name, $version ) = @_;
+  $version = $self->max_version($name) unless $version;
 
-    $self->logger->("Getting $name,$version");
-    $self->setError();
+  $self->logger->("Getting $name,$version");
+  $self->setError();
 
-    unless ($self->defined($name,$version)) {
-        $self->setError("$self->{DirName}/$name,$version does not exist");
-        return;
-    }
+  unless ( $self->defined( $name, $version ) ) {
+    $self->setError("$self->{DirName}/$name,$version does not exist");
+    return;
+  }
 
-    # This shouldn't happen, unless someone's been fiddling with the database.
-    my $handle;
-    unless(open($handle, "<", $self->{DirName} . "/" . $name . "," . $version)) {
-        $self->setError("Unreadable file $self->{DirName}/$name,$version: $!");
-        return;
-    }
+  # This shouldn't happen, unless someone's been fiddling with the database.
+  my $handle;
+  unless (
+    open( $handle, "<", $self->{DirName} . "/" . $name . "," . $version ) ) {
+    $self->setError("Unreadable file $self->{DirName}/$name,$version: $!");
+    return;
+  }
 
-    # Read the file and uncollapse the data into a hash again.
-    my %oldhash;
-    my @flat = <$handle>;
-    $self->logger->("Read the raw file");
-    close($handle);
-    my $flat = join("",@flat);
-    my %fluffy = %{Storable->inflate(\$flat)};
-    return %fluffy;
+  # Read the file and uncollapse the data into a hash again.
+  my %oldhash;
+  my @flat = <$handle>;
+  $self->logger->("Read the raw file");
+  close($handle);
+  my $flat   = join( "", @flat );
+  my %fluffy = %{ Storable->inflate( \$flat ) };
+  return %fluffy;
 }
 
 =head2 put ($name, $contentsref, $version)
@@ -233,37 +235,36 @@ Store an entry.
 =cut
 
 sub put {
-    my ($self, $name, $contents, $version) = @_;
+  my ( $self, $name, $contents, $version ) = @_;
 
-    # Fix up the version.
-    if ($version eq "") {
-        $version = 1;
-    }
-    else {
-        $version += 0;		# Force to numeric
-    }
+  # Fix up the version.
+  if ( $version eq "" ) {
+    $version = 1;
+  } else {
+    $version += 0;    # Force to numeric
+  }
 
-    $self->setError();
+  $self->setError();
 
-    # Open the file, if we can.
-    my $target = "$self->{DirName}/$name,$version";
-    my $handle;
-    unless(open($handle, ">", "$target")) {
-        $self->setError("Cannot write to $target: $!");
-        return;
-    }
+  # Open the file, if we can.
+  my $target = "$self->{DirName}/$name,$version";
+  my $handle;
+  unless ( open( $handle, ">", "$target" ) ) {
+    $self->setError("Cannot write to $target: $!");
+    return;
+  }
 
-    # Flatten and store.
-    my $flat = Storable->flatten($contents);
-    my $oldfh = select($handle);
-    print $handle $flat;
-    select $oldfh;
-    close $handle;
+  # Flatten and store.
+  my $flat  = Storable->flatten($contents);
+  my $oldfh = select($handle);
+  print $handle $flat;
+  select $oldfh;
+  close $handle;
 
-    # ensure it is group-writeable (insert-mail support).
-    chmod 0664,$target;
+  # ensure it is group-writeable (insert-mail support).
+  chmod 0664, $target;
 
-    return 1;
+  return 1;
 }
 
 =head2 delete ($title)
@@ -273,26 +274,25 @@ Remove an entry; use most recent version if none supplied
 =cut
 
 sub delete {
-    my($self,$name,$version) = @_;
+  my ( $self, $name, $version ) = @_;
 
-    # Fix up the version.
-    if ($version eq undef) {
-        $version = $self->max_version($name);
-    }
-    else {
-        $version += 0 if $version ne "";	# Force to numeric if needed
-    }
+  # Fix up the version.
+  if ( $version eq undef ) {
+    $version = $self->max_version($name);
+  } else {
+    $version += 0 if $version ne "";    # Force to numeric if needed
+  }
 
-    # Report an error if the version doesn't exist.
-    unless ($self->defined($name,$version)) {
-        $self->setError("$self->{DirName}/$name,$version does not exist");
-        return;
-    }
+  # Report an error if the version doesn't exist.
+  unless ( $self->defined( $name, $version ) ) {
+    $self->setError("$self->{DirName}/$name,$version does not exist");
+    return;
+  }
 
-    # Do it.
-    $self->setError();
-    my $file = "$self->{DirName}/$name,$version";
-    unlink $file if (-e $file);
+  # Do it.
+  $self->setError();
+  my $file = "$self->{DirName}/$name,$version";
+  unlink $file if ( -e $file );
 }
 
 =head2 purge($title) - remove all versions of an entry
@@ -300,19 +300,19 @@ sub delete {
 =cut
 
 sub purge {
-    my($self,$name) = @_;
+  my ( $self, $name ) = @_;
 
-    # Rewind so we see all the files.
-    $self->setError();
-    $self->dh_reset or return 0;
+  # Rewind so we see all the files.
+  $self->setError();
+  $self->dh_reset or return 0;
 
-    # Note fancy implied loop done by map. readdir() is evaluated in list
-    # context, so it returns all names in the directory.
-    map unlink($self->{DirName} . "/" . $_), # remove files ...
-               grep(/^$name,/,		      # ... matching this ...
-                    readdir $self->{Handle});     # ... in list of files in dir
-    $self->{Rewound} = 0;
-    return 1;
+  # Note fancy implied loop done by map. readdir() is evaluated in list
+  # context, so it returns all names in the directory.
+  map unlink( $self->{DirName} . "/" . $_ ),    # remove files ...
+    grep( /^$name,/,                            # ... matching this ...
+    readdir $self->{Handle} );                  # ... in list of files in dir
+  $self->{Rewound} = 0;
+  return 1;
 }
 
 =head2 iterator()
@@ -322,32 +322,32 @@ Return a list of all highest-version keys.
 =cut
 
 sub iterator {
-    my($self) = shift;
+  my ($self) = shift;
 
-    # Get list of all names.
-    $self->setError();
-    $self->dh_reset or return;    # undefined value
+  # Get list of all names.
+  $self->setError();
+  $self->dh_reset or return;    # undefined value
 
-    my (@names) = readdir $self->{Handle};
-    $self->{Rewound} = 0;
+  my (@names) = readdir $self->{Handle};
+  $self->{Rewound} = 0;
 
-    # Scan through names, returning highest-numbered version for each.
-    my %highest = ();
-    foreach (@names) {
-        $^W=0;
-        my($name,$version) = /^(.*),(.*)/;
-        if ($highest{$name} < $version) {
-            $highest{$name} = $version;
-        }
-        $^W=1;
+  # Scan through names, returning highest-numbered version for each.
+  my %highest = ();
+  foreach (@names) {
+    $^W = 0;
+    my ( $name, $version ) = /^(.*),(.*)/;
+    if ( $highest{$name} < $version ) {
+      $highest{$name} = $version;
     }
+    $^W = 1;
+  }
 
-    #  Build iterator as "name,version" entries.
-    @names = ();
-    foreach (keys %highest) {
-        push @names,"$_,$highest{$_}";
-    }
-    return @names;
+  #  Build iterator as "name,version" entries.
+  @names = ();
+  foreach ( keys %highest ) {
+    push @names, "$_,$highest{$_}";
+  }
+  return @names;
 }
 
 =head2 logger(REF)
@@ -357,9 +357,9 @@ Set calback for message handling
 =cut
 
 sub logger {
-   my $self = shift;
-   $self->{_logger} = shift if int @_;
-   $self->{_logger};
+  my $self = shift;
+  $self->{_logger} = shift if int @_;
+  $self->{_logger};
 }
 
 =head2 fatal(REF)
@@ -369,9 +369,9 @@ Set calback for message handling
 =cut
 
 sub fatal {
-   my $self = shift;
-   $self->{_fatal} = shift if int @_;
-   $self->{_fatal};
+  my $self = shift;
+  $self->{_fatal} = shift if int @_;
+  $self->{_fatal};
 }
 
 =head2 setError($msg, $msg, $msg ...)
@@ -381,10 +381,10 @@ Global error message capture for this instance
 =cut
 
 sub setError {
-    my ($self) = shift;
-    $self->{ErrorMsg} = join(" ",@_);
-    $self->logger->($self->{ErrorMsg}) if int @_;
-    return 1;
+  my ($self) = shift;
+  $self->{ErrorMsg} = join( " ", @_ );
+  $self->logger->( $self->{ErrorMsg} ) if int @_;
+  return 1;
 }
 
 =head2  getError()
@@ -394,8 +394,8 @@ Fetch last error that occurred.
 =cut
 
 sub getError {
-    my($self) = @_;
-    return $self->{ErrorMsg};
+  my ($self) = @_;
+  return $self->{ErrorMsg};
 }
 
 =head2 lock_limit()
@@ -405,9 +405,9 @@ Set/get the number of seconds to wait for a lock
 =cut
 
 sub lock_limit {
-    my ($self,$limit) = @_;
-    $self->{LockLimit} = $limit if defined $limit;
-    $self->{LockLimit};
+  my ( $self, $limit ) = @_;
+  $self->{LockLimit} = $limit if defined $limit;
+  $self->{LockLimit};
 }
 
 =head2 dh_reset
@@ -420,22 +420,21 @@ Returns true if success or false if failure.
 =cut
 
 sub dh_reset {
-    my $self = shift;
-    my $success = 1;
+  my $self    = shift;
+  my $success = 1;
 
-    if ($^O eq 'MSWin32') {
-	      closedir($self->{Handle});
-	      opendir($self->{Handle},$self->{DirName}) or
-	      $success = 0;
-    }
-    else {
-	      rewinddir $self->{Handle};
-    }
-    $success;
+  if ( $^O eq 'MSWin32' ) {
+    closedir( $self->{Handle} );
+    opendir( $self->{Handle}, $self->{DirName} )
+      or $success = 0;
+  } else {
+    rewinddir $self->{Handle};
+  }
+  $success;
 }
 
 sub _locker {
-  my( $self, $locker_object ) = @_;
+  my ( $self, $locker_object ) = @_;
   $self->{_locker} = $locker_object if defined $locker_object;
   $self->{_locker};
 }
