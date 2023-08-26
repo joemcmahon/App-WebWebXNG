b8df5d168a6857f76130aada16adaf822aad79cc
Joe McMahon <joe.mcmahon@gmail.com>
2023-08-26 09:35:27 -0700

    Added minimum POD to pass tests.

--- lib/App/WebWebXNG.pm
+++ lib/App/WebWebXNG.pm
@@ -1,7 +1,37 @@
+#ABSTRACT: A Perl wiki with some useful bells and whistles
+
+package App::WebWebXNG;
+
+=head1 SYNOPSIS
+
+    # Handwaving the initialization, which will be needed...
+    use App::WebWebXNG;
+    my $wiki = App::WebWebXNG->new();
+    $wiki->run;
+
+=head1 DESCRIPTION
+
+C<App::WebWebXNG> is a minimal wiki implemented in Perl. It concentrates on
+providing easy-to-use access control over media types. This version is a
+port of the original WebWebX from 1998 to a modern Perl stack. It is hoped
+that this will, over time, provide a useful basic wiki that can be deployed
+easily pretty much anywhere Perl can be installed.
+
+=cut
+
+
 use 5.38.0;
 use Mojo::Base 'Mojolicious', -signatures;
 
-sub main {
+=head1 CLASS METHODS
+
+=head2 run
+
+Called to launch the application.
+
+=cut
+
+sub run {
 }
 
-main unless caller();
+run unless caller();
