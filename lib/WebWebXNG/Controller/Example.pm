eccd85c2697f5a5ee65729e9e7ebdda7b645b544
Joe McMahon <joe.mcmahon@gmail.com>
2023-08-26 14:30:23 -0700

    Remove outmoded controller; POD coverage; criticrc
     - Move the perlcritic rc file to the location expected by `Dist::Zilla`
     - Minimum amount of POD to pass coverage
     - Work around signature support problems in Perl::Critic

--- lib/WebWebXNG/Controller/Example.pm
+++ lib/WebWebXNG/Controller/Example.pm
@@ -2,6 +2,14 @@ package WebWebXNG::Controller::Example;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 
 # This action will render a template
+=head1 INSTANCE METHODS
+
+=head2 welcome
+
+Renders the dummy page for development.
+
+=cut
+
 sub welcome ($self) {
 
   # Render template "example/welcome.html.ep" with message
