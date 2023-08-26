f466229958bc529315188ac8d75c18a6ea5741c9
Joe McMahon <joe.mcmahon@gmail.com>
2023-08-26 11:10:09 -0700

    Move route setup from defunct module to main
    The only thing the old App::WebWebXNG module was doing was setting up
    the dummy routes for Mojo. Just move that code into lib/WebWebXNG.pm and
    get rid of the old module.

--- lib/WebWebXNG.pm
+++ lib/WebWebXNG.pm
@@ -1,6 +1,8 @@
 package WebWebXNG;
 use Mojo::Base 'Mojolicious', -signatures;
 
+use App::WebWebXNG;
+
 # This method will run once at server start
 sub startup ($self) {
 
@@ -15,6 +17,31 @@ sub startup ($self) {
 
   # Normal route to controller
   $r->get('/')->to('Example#welcome');
+
+  # Add WebWebXNG stub routes.
+  $r->get('/SearchRefs')->to("Example#welcome");    #HandleSearch
+  $r->get('/ViewPage')->to("Example#welcome");      #HandleView
+  $r->get('/ShowDiffs')->to("Example#welcome");     #HandleDiffs
+  $r->get('/EditLinks')->to("Example#welcome");     #HandleLinks
+  $r->get('/EditLink')->to("Example#welcome");      #HandleEditLink
+  $r->get('/EditPage')->to("Example#welcome");      #HandleEdit
+  $r->get('/RestorePage')->to("Example#welcome");   #HandleRestore
+  $r->get('/PurgePage')->to("Example#welcome");     #HandlePurge
+  $r->get('/PageProps')->to("Example#welcome");     #HandleProperties
+  $r->get('/PageInfo')->to("Example#welcome");      #HandleInfo
+  $r->get('/RenamePage')->to("Example#welcome");    #HandleRename
+  $r->get('/DeletePage')->to("Example#welcome");    #HandleDelete
+  $r->get('/MailNotify')->to("Example#welcome");    #HandleMail
+  $r->get('/EditMail')->to("Example#welcome");      #HandleEditMail
+  $r->get('/RecentChanges')->to("Example#welcome"); #HandleRecentChanges
+  $r->get('/SetAdminData')->to("Example#welcome");  #EditAdminRecord
+  $r->get('/UnlockFile')->to("Example#welcome");    #UnlockFile
+  $r->get('/ManageUsers')->to("Example#welcome");   #ManageUsers
+  $r->get('/UserPWChange')->to("Example#welcome");  #UserPWChange
+  $r->get('/GlobalPurge')->to("Example#welcome");   #HandleGlobalPurge
+
+  # All of these are probably also going to need POST endpoints as well.
+  # Adding only the GETs for now.
 }
 
 1;
